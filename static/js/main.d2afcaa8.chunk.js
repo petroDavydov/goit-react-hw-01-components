6787f9e602bd9152d6d74f1ba056af1fa77a675e
petroDavydov <petrodavydov@gmail.com>
2021-10-01 13:54:47 +0300

    Updates

---
+++ static/js/main.d2afcaa8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],{11:function(t,n,e){"use strict";e.r(n);var c=e(1),o=e.n(c),r=e(3),i=e.n(r),s=(e(8),e(9),e(0));var a=function(){return Object(s.jsx)("div",{children:"Hello world"})};i.a.render(Object(s.jsx)(o.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(t,n,e){},9:function(t,n,e){}},[[11,1,2]]]);
+//# sourceMappingURL=main.d2afcaa8.chunk.js.map
