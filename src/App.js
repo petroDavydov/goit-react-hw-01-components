d6b6d6547130a0c92d78b8de298fc90b8badff63
petroDavydov <petrodavydov@gmail.com>
2021-10-02 21:51:17 +0300

    add transaction

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import statisticalData from './components/statistics/statistical-data.json';
 import FriendList from './components/friends/FriendsList';
 import friends from './components/friends/friends.json';
 // ============================================
-
+import TransactionHistory from './components/transaction/TransactionHistory';
+import transactions from './components/transaction/transactions.json';
 // ============================================
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
         <Statistics title="Upload stats" stats={statisticalData} />;
         <Statistics stats={statisticalData} />;
         <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />;
       </Container>
     </div>
   );
---
+++ src/components/transaction/TransactionHistory.jsx
@@ -0,0 +1,44 @@
+import './TransactionHistory.module.css';
+import PropTypes from 'prop-types';
+
+export default function TransactionHistory({ items }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(item => (
+          <tr key={item.id}>
+            <td>{item.type}</td>
+            <td>{item.amount}</td>
+            <td>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+TransactionHistory.defaultProps = {
+  id: 'Random transaction 25-36 numbers',
+  type: 'invoice,payment,withdrawal,deposit ets.',
+  amount: 'Amount: 000.00',
+  currency: 'Currency',
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    }),
+  ),
+};
